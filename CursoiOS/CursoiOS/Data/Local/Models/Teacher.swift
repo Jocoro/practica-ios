4d7c36f8d277a910116a5145fe921da5d68b5914
Jorge Corugedo Rodriguez <jocuniversal@gmail.com>
2019-09-20 20:58:50 +0200

    Proffesor Type

--- CursoiOS/CursoiOS/Data/Local/Models/Teacher.swift
+++ CursoiOS/CursoiOS/Data/Local/Models/Teacher.swift
@@ -7,6 +7,41 @@
 //
 
 import Foundation
-class Teacher{
+enum TeacherType: String {
+    case intern
+    case extern
+    var description: String {
+        switch self{
+        case .intern:
+            return "Interno"
+            
+        case .extern:
+            return "Externo"
+            
+        }
+    }
+}
+class Teacher: Hashable, CustomStringConvertible {
     
+    
+    static func == (lhs: Teacher, rhs: Teacher) -> Bool {
+        return ObjectIdentifier(lhs) == ObjectIdentifier(rhs)
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        return hasher.combine(ObjectIdentifier(self))
+    }
+    var name: String?
+    var email: String?
+    var type: TeacherType?
+    
+    var description: String {
+        return "Teacher data: \(String(describing: name)) \(String(describing: email))"
+    }
+    convenience init(name: String, email: String? = nil, type: TeacherType? = nil) {
+        self.init()
+        self.name = name
+        self.email = email
+        self.type = type
+    }
 }
